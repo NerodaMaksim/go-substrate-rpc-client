c26ded09d8898fe54a9809504c75bc7df305cc2d
Maksim Neroda <m.neroda@inc4.net>
2024-02-08 15:23:25 +0200

    Test commit

---
+++ client/client.go
@@ -0,0 +1,109 @@
+// Go Substrate RPC Client (GSRPC) provides APIs and types around Polkadot and any Substrate-based chain RPC calls
+//
+// Copyright 2019 Centrifuge GmbH
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package client
+
+import (
+	"context"
+	"log"
+	"time"
+
+	"github.com/centrifuge/go-substrate-rpc-client/v4/config"
+	gethrpc "github.com/centrifuge/go-substrate-rpc-client/v4/gethrpc"
+	"github.com/centrifuge/go-substrate-rpc-client/v4/types"
+	"github.com/centrifuge/go-substrate-rpc-client/v4/types/codec"
+)
+
+//go:generate mockery --name Client
+
+type Client interface {
+	// Call makes the call to RPC method with the provided args
+	// args must be encoded in the format RPC understands
+	Call(result interface{}, method string, args ...interface{}) error
+
+	Subscribe(ctx context.Context, namespace, subscribeMethodSuffix, unsubscribeMethodSuffix,
+		notificationMethodSuffix string, channel interface{}, args ...interface{}) (
+		*gethrpc.ClientSubscription, error)
+
+	URL() string
+
+	Close()
+}
+
+type client struct {
+	gethrpc.Client
+
+	url string
+}
+
+// URL returns the URL the client connects to
+func (c client) URL() string {
+	return c.url
+}
+
+func (c client) Close() {
+	c.Client.Close()
+}
+
+// Connect connects to the provided url
+func Connect(url string) (Client, error) {
+	log.Printf("Connecting to %v...", url)
+
+	ctx, cancel := context.WithTimeout(context.Background(), config.Default().DialTimeout)
+	defer cancel()
+
+	c, err := gethrpc.DialContext(ctx, url, false, nil, time.Duration(0), time.Duration(0))
+	if err != nil {
+		return nil, err
+	}
+	cc := client{*c, url}
+	return &cc, nil
+}
+
+// ConnectWithHTTPClient connects to the provided url with a custom http client
+func ConnectWithHTTPClient(url string, insecureSkipVerify bool, headers map[string]string, connectionTimeout, wsReadDeadline time.Duration) (Client, error) {
+	log.Printf("Connecting to %v...", url)
+
+	ctx, cancel := context.WithTimeout(context.Background(), config.Default().DialTimeout)
+	defer cancel()
+
+	c, err := gethrpc.DialContext(ctx, url, insecureSkipVerify, headers, connectionTimeout, wsReadDeadline)
+	if err != nil {
+		return nil, err
+	}
+	cc := client{*c, url}
+	return &cc, nil
+}
+
+func CallWithBlockHash(c Client, target interface{}, method string, blockHash *types.Hash, args ...interface{}) error {
+	if blockHash == nil {
+		err := c.Call(target, method, args...)
+		if err != nil {
+			return err
+		}
+		return nil
+	}
+	hexHash, err := codec.Hex(*blockHash)
+	if err != nil {
+		return err
+	}
+	args = append(args, hexHash)
+	err = c.Call(target, method, args...)
+	if err != nil {
+		return err
+	}
+	return nil
+}
